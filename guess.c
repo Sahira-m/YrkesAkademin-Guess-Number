c4f5d49ebde9422267bc03a7e43fe88ecfabed13
Sahira-m <sahikmh@gmail.com>
2023-02-26 14:46:38 +0100

    bug cleared

--- guess.c
+++ guess.c
@@ -1,56 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include<ctype.h>
-#include<conio.h>
-//Main function
-void main() 
+#include <ctype.h>
+#include <conio.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+#define Yes y | Y
+#define No n | N
+// function to check the input is string or number
+char Isnumbercheck(char *guess)
 {
-  char playAgain[4];
-  int guess, num, numTries;
+  char isnumber = TRUE;
+  int len = strlen(guess);
+  int i;
+  for (i = 0; i < len; i++)
+    if (!isdigit(guess[i]))
+    {
+      isnumber = FALSE;
+      break;
+    }
+
+  return isnumber;
+}
+// Main function
+void main()
+{
+  char playAgain[4], isnumber = FALSE;
+  char guess[3];
+  int randomNumber, numTries;
   srand(time(NULL));
-  do 
+  /*Generate random number*/
+  randomNumber = rand() % 100 + 1;
+
+  do
   {
-    numTries = 1;
-    num = rand() % 100 + 1;
+
     printf("I've chosen a number between 1 and 100. Can you guess what it is?\n");
-    do {
+    numTries = 1; /*Initialize the Num of entries to 1*/
+
+    while (numTries < 5)
+    {
       printf("Guess %d: ", numTries);
-      scanf("%d", &guess);
-      if(isdigit(guess)!=0)
+      scanf("%s", &guess);
+      isnumber = Isnumbercheck(guess);
+      if (isnumber == FALSE)
       {
-        printf("it is a string please enter a number again");
-        scanf("%d", &guess);
-        return;
+        printf("Entered input is not a number\n");
       }
       else
       {
- numTries++;
-   if(guess>0 && guess <100)
+        int guessNumber = atoi(guess); /*Converting string to number*/
+
+        if (guessNumber > 0 && guessNumber < 100)
         {
-           if(guess==num)
-      {
-        printf("Right! You guessed right in %d tries.", numTries);
-        break;
-      }
-      else if (guess > num) 
-        printf("The number is Smaller.\n");
-        
-       else if (guess < num) 
-         printf("The number is Larger.\n");  
-       
-       }
- else
-       printf("please choose a number between 1 and 100");
+          if (guessNumber == randomNumber)
+          {
+            printf("Right! You guessed right in %d tries.", numTries);
+            break;
+          }
+          else if (guessNumber > randomNumber)
+            printf("The number is Smaller.\n");
+
+          else if (guessNumber < randomNumber)
+            printf("The number is Larger.\n");
+          numTries++;
+        }
+        else
+          printf("\n please choose a number between 1 and 100");
       }
     }
-    while (numTries!=5);
-    printf("Do you want to play again (Yes/No)? ");
+    printf("\n Guess number is %d:", randomNumber);
+    printf("\n Do you want to play again (Yes/No)? ");
     scanf("%s", playAgain);
-  }
-
- while (playAgain[0] == 'Y' || playAgain[0] == 'y');
-  printf("Thanks for this time!\n");
-  getch();
+  } while (playAgain[0] == 'Y' || playAgain[0] == 'y');
 }
-
